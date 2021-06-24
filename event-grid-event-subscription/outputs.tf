ba52872719db422d1afe9923d3c1a9d863c4929f
Daniel Bjerring Jørgensen <dbj1986dk@gmail.com>
2021-06-24 14:53:24 +0200

    update outputs of event grid subscription

--- event-grid-event-subscription/outputs.tf
+++ event-grid-event-subscription/outputs.tf
@@ -16,22 +16,6 @@ output "id" {
   description = "The EventGrid Topic ID."
 }
 
-output endpoint {
-  value = azurerm_eventgrid_topic.main.endpoint
-  description = " The Endpoint associated with the EventGrid Topic."
-}
-
-output "primary_access_key" {
-  value       = azurerm_eventgrid_topic.main.primary_access_key
-  description = "The Primary Shared Access Key associated with the EventGrid Topic."
-  sensitive   = true
-}
-
-output "secondary_access_key" {
-  value       = azurerm_eventgrid_topic.main.secondary_access_key
-  description = "The Secondary Shared Access Key associated with the EventGrid Topic."
-}
-
 output "dependent_on" {
   value       = null_resource.dependency_setter.id
   description = "The dependencies of the Event Grid Topic component."
