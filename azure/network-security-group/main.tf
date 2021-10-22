12eb6955205fe42a36fda3fd3af8eeabf496c175
Martin Schmidt <martin@scoding.dk>
2021-10-22 09:47:05 +0000

    updated nsg

--- azure/network-security-group/main.tf
+++ azure/network-security-group/main.tf
@@ -37,20 +37,6 @@ resource "azurerm_network_security_group" "this" {
   resource_group_name = var.resource_group_name
   tags                = merge(var.tags, local.module_tags)
 
-
-  security_rule {
-    for_each = {for rule in var.security_rules:  rule.name => rule} 
-    name                       = each.value.name
-    priority                   = each.value.priority
-    direction                  = each.value.direction
-    access                     = each.value.access
-    protocol                   = each.value.protocol
-    source_port_range          = each.value.source_port_range
-    destination_port_range     = each.value.destination_port_range
-    source_address_prefix      = each.value.source_address_prefix
-    destination_address_prefix = each.value.destination_address_prefix
-  }
-
   lifecycle {
     ignore_changes = [
       # Ignore changes to tags, e.g. because a management agent
@@ -58,4 +44,20 @@ resource "azurerm_network_security_group" "this" {
       tags,
     ]
   }
+}
+
+resource "azurerm_network_security_rule" "this" {
+  for_each = { for rule in var.security_rules : rule.name => rule }
+
+  resource_group_name         = var.resource_group_name
+  network_security_group_name = azurerm_network_security_group.this.name
+  name                        = each.value.name
+  priority                    = each.value.priority
+  direction                   = each.value.direction
+  access                      = each.value.access
+  protocol                    = each.value.protocol
+  source_port_range           = each.value.source_port_range
+  destination_port_range      = each.value.destination_port_range
+  source_address_prefix       = each.value.source_address_prefix
+  destination_address_prefix  = each.value.destination_address_prefix
 }
