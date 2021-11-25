702bcc2b97220598d0899bde96d34ec9f26bc8de
Martin Schmidt <martin@scoding.dk>
2021-11-25 07:23:45 +0000

    Updated security-rules to not remove rules

--- azure/network-security-group/main.tf
+++ azure/network-security-group/main.tf
@@ -36,7 +36,19 @@ resource "azurerm_network_security_group" "this" {
   location            = var.location
   resource_group_name = var.resource_group_name
   tags                = merge(var.tags, local.module_tags)
-  security_rule       = []
+
+  security_rule {
+    for_each = { for rule in var.security_rules : rule.name => rule }
+    name                        = each.value.name
+    priority                    = each.value.priority
+    direction                   = each.value.direction
+    access                      = each.value.access
+    protocol                    = each.value.protocol
+    source_port_range           = each.value.source_port_range
+    destination_port_range      = each.value.destination_port_range
+    source_address_prefix       = each.value.source_address_prefix
+    destination_address_prefix  = each.value.destination_address_prefix
+  }
 
   lifecycle {
     ignore_changes = [
@@ -45,20 +57,4 @@ resource "azurerm_network_security_group" "this" {
       tags,
     ]
   }
-}
-
-resource "azurerm_network_security_rule" "this" {
-  for_each = { for rule in var.security_rules : rule.name => rule }
-
-  resource_group_name         = var.resource_group_name
-  network_security_group_name = azurerm_network_security_group.this.name
-  name                        = each.value.name
-  priority                    = each.value.priority
-  direction                   = each.value.direction
-  access                      = each.value.access
-  protocol                    = each.value.protocol
-  source_port_range           = each.value.source_port_range
-  destination_port_range      = each.value.destination_port_range
-  source_address_prefix       = each.value.source_address_prefix
-  destination_address_prefix  = each.value.destination_address_prefix
 }
