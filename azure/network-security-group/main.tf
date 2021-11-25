969a636799abd73af93e08aa694871cac36dd0e8
Martin Schmidt <martin@scoding.dk>
2021-11-25 07:31:18 +0000

    changed structure nsg to dynamic

--- azure/network-security-group/main.tf
+++ azure/network-security-group/main.tf
@@ -37,17 +37,19 @@ resource "azurerm_network_security_group" "this" {
   resource_group_name = var.resource_group_name
   tags                = merge(var.tags, local.module_tags)
 
-  security_rule {
+  dynamic "security_rule" {
     for_each = { for rule in var.security_rules : rule.name => rule }
-    name                        = each.value.name
-    priority                    = each.value.priority
-    direction                   = each.value.direction
-    access                      = each.value.access
-    protocol                    = each.value.protocol
-    source_port_range           = each.value.source_port_range
-    destination_port_range      = each.value.destination_port_range
-    source_address_prefix       = each.value.source_address_prefix
-    destination_address_prefix  = each.value.destination_address_prefix
+    content {
+      name                        = each.value.name
+      priority                    = each.value.priority
+      direction                   = each.value.direction
+      access                      = each.value.access
+      protocol                    = each.value.protocol
+      source_port_range           = each.value.source_port_range
+      destination_port_range      = each.value.destination_port_range
+      source_address_prefix       = each.value.source_address_prefix
+      destination_address_prefix  = each.value.destination_address_prefix
+    }
   }
 
   lifecycle {
