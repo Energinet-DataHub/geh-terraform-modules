4855b177a19bd3ecb8c43bdd613898c4ecc8fc1b
michaelmajgaard <52096428+michaelmajgaard@users.noreply.github.com>
2021-12-14 16:24:51 +0100

    Added requires_duplicate_detection to service-bus-queue module.

--- azure/service-bus-queue/main.tf
+++ azure/service-bus-queue/main.tf
@@ -12,9 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 resource "azurerm_servicebus_queue" "this" {
-  name                = lower(var.name)
-  resource_group_name = var.resource_group_name
-  namespace_name      = var.namespace_name
-  requires_session    = var.requires_session
-  enable_partitioning = true
+  name                         = lower(var.name)
+  resource_group_name          = var.resource_group_name
+  namespace_name               = var.namespace_name
+  requires_session             = var.requires_session
+  requires_duplicate_detection = var.requires_duplicate_detection
+  enable_partitioning          = true
 }
