13b80a010113403d6c99c0cce8cb43ace5cbb646
Kristian Schneider <khs@thamesconsulting.com>
2022-01-11 09:33:02 +0100

    .

--- azure/storage-account/main.tf
+++ azure/storage-account/main.tf
@@ -108,7 +108,7 @@ resource "azurerm_private_dns_a_record" "this" {
 # Link the Private Zone with the VNet
 resource "azurerm_private_dns_zone_virtual_network_link" "this" {
   name                  = "${azurerm_storage_account.this.name}link"
-  resource_group_name   = azurerm_resource_group.rg.name
+  resource_group_name   = var.resource_group_name
   private_dns_zone_name = azurerm_private_dns_zone.this.name
   virtual_network_id    = var.vnet_id
 }
