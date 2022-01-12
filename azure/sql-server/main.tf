a80cc0b660234965b57d4a6ef3ec6d6e09869417
Kristian Schneider <khs@thamesconsulting.com>
2022-01-12 13:29:32 +0100

    .

--- azure/sql-server/main.tf
+++ azure/sql-server/main.tf
@@ -25,7 +25,7 @@ resource "azurerm_mssql_server" "this" {
   version                       = var.sql_version
   administrator_login           = var.administrator_login
   administrator_login_password  = var.administrator_login_password
-  public_network_access_enabled = false
+  public_network_access_enabled = true
   identity {
     type  = "SystemAssigned" 
   }
@@ -41,12 +41,12 @@ resource "azurerm_mssql_server" "this" {
   }
 }
 
-#resource "azurerm_sql_virtual_network_rule" "sqlvnetrule" {
-#  name                = "sql-vnet-rule"
-#  resource_group_name = var.resource_group_name
-#  server_name         = azurerm_mssql_server.this.name
-#  subnet_id           = var.private_endpoint_subnet_id
-#}
+resource "azurerm_sql_virtual_network_rule" "sqlvnetrule" {
+ name                = "sql-vnet-rule"
+ resource_group_name = var.resource_group_name
+ server_name         = azurerm_mssql_server.this.name
+ subnet_id           = var.private_endpoint_subnet_id
+}
 
 
 resource "azurerm_private_endpoint" "this" {
