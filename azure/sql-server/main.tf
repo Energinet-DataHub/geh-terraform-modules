e8d01c4eaf742c693214c2d0347e415c90e8219b
Kristian Schneider <khs@thamesconsulting.com>
2022-01-17 09:16:48 +0100

    Update azure/sql-server/main.tf
    Co-authored-by: djorgensendk <32477395+djorgensendk@users.noreply.github.com>

--- azure/sql-server/main.tf
+++ azure/sql-server/main.tf
@@ -47,7 +47,7 @@ resource "azurerm_private_endpoint" "this" {
    resource_group_name = var.resource_group_name
    subnet_id           = var.private_endpoint_subnet_id
    private_service_connection {
-     name                           = "psc${lower(var.name)}${lower(var.project_name)}${lower(var.environment_short)}${lower(var.environment_instance)}"
+     name                           = "psc-${lower(var.name)}${lower(var.project_name)}${lower(var.environment_short)}${lower(var.environment_instance)}"
      private_connection_resource_id = azurerm_mssql_server.this.id
      is_manual_connection           = false
      subresource_names              = ["sqlServer"]
