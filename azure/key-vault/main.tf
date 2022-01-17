de94c464b9974d84bf11e64cbee6f1625c367f6d
Kristian Schneider <khs@thamesconsulting.com>
2022-01-17 11:27:41 +0100

    a record

--- azure/key-vault/main.tf
+++ azure/key-vault/main.tf
@@ -66,6 +66,16 @@ resource "azurerm_private_endpoint" "this" {
   }
 }
 
+# Create an A record pointing to the keyvalut private endpoint
+resource "azurerm_private_dns_a_record" "this" {
+  name                = azurerm_key_vault.this.name
+  zone_name           = var.private_dns_zone_name
+  resource_group_name = var.resource_group_name
+  ttl                 = 3600
+  records             = [azurerm_private_endpoint.this.private_service_connection[0].private_ip_address]
+}
+
+
 resource "azurerm_key_vault_access_policy" "selfpermissions" {
   key_vault_id            = azurerm_key_vault.this.id
   tenant_id               = data.azurerm_client_config.this.tenant_id
