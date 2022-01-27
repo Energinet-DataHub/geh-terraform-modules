f194f95fd9d791e301743b9971c826b439c3ff61
René Nielsen <renetdev@gmail.com>
2022-01-27 13:13:18 +0100

    test remote vnet peering

--- azure/vnet/main.tf
+++ azure/vnet/main.tf
@@ -55,7 +55,7 @@ resource "azurerm_virtual_network_peering" "remote" {
 
   name                          = "test_remote"
   #"${lower(try(var.peerings[count.index].name, null))}-to-${local.NAME}"
-  resource_group_name           = try(var.peerings[count.index].remote_virtual_network_resource_group_name, null)
+  resource_group_name           = "test"
   virtual_network_name          = try(var.peerings[count.index].remote_virtual_network_id, null)
   remote_virtual_network_id     = "/subscriptions/${try(var.peerings[count.index].remote_virtual_network_subscription_id, null)}/resourceGroups/${var.resource_group_name}/providers/Microsoft.Network/virtualNetworks/${azurerm_virtual_network.this.name}"
   allow_virtual_network_access  = true
