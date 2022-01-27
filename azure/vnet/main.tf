768a6cd81df94ecc009e11fd2f9579286acc4eff
René Nielsen <renetdev@gmail.com>
2022-01-27 12:45:06 +0100

    changed variable creation in vnet

--- azure/vnet/main.tf
+++ azure/vnet/main.tf
@@ -56,6 +56,6 @@ resource "azurerm_virtual_network_peering" "remote" {
   name                          = "${lower(try(var.peerings[count.index].name, null))}-to-${local.NAME}"
   resource_group_name           = try(var.peerings[count.index].remote_virtual_network_resource_group_name, null)
   virtual_network_name          = try(var.peerings[count.index].remote_virtual_network_id, null)
-  remote_virtual_network_id     = "/subscriptions/${try(var.peerings[count.index].remote_virtual_network_subscription_id, null)}/resourceGroups/${try(var.peerings[count.index].remote_virtual_network_resource_group_name, null)}/providers/Microsoft.Network/virtualNetworks/${azurerm_virtual_network.this.name}"
+  remote_virtual_network_id     = "/subscriptions/${try(var.peerings[count.index].remote_virtual_network_subscription_id, null)}/resourceGroups/${var.resource_group_name}/providers/Microsoft.Network/virtualNetworks/${azurerm_virtual_network.this.name}"
   allow_virtual_network_access  = true
 }
