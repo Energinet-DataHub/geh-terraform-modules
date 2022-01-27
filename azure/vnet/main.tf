323b933352796fce2367f2903ad649d856bf6f0e
René Nielsen <renetdev@gmail.com>
2022-01-27 13:28:28 +0100

    revert test changes vnet

--- azure/vnet/main.tf
+++ azure/vnet/main.tf
@@ -53,10 +53,9 @@ resource "azurerm_virtual_network_peering" "local" {
 resource "azurerm_virtual_network_peering" "remote" {
   count                         = length(var.peerings)
 
-  name                          = "test_remote"
-  #"${lower(try(var.peerings[count.index].name, null))}-to-${local.NAME}"
+  name                          = "${lower(try(var.peerings[count.index].name, null))}-to-${local.NAME}"
   resource_group_name           = try(var.peerings[count.index].remote_virtual_network_resource_group_name, null)
-  virtual_network_name          = "test_1"
+  virtual_network_name          = try(var.peerings[count.index].remote_virtual_network_id, null)
   remote_virtual_network_id     = "/subscriptions/${try(var.peerings[count.index].remote_virtual_network_subscription_id, null)}/resourceGroups/${var.resource_group_name}/providers/Microsoft.Network/virtualNetworks/${azurerm_virtual_network.this.name}"
   allow_virtual_network_access  = true
 }
