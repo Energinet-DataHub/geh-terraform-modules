b4bb8dbeb99c4addf30f0a16e346e487202c1c89
Dan Stenrøjl <XDAST@energinet.dk>
2022-01-27 10:10:04 +0100

    What: Update servicebus to avoid "deprecated" warning

--- azure/service-bus-queue/variables.tf
+++ azure/service-bus-queue/variables.tf
@@ -16,14 +16,9 @@ variable name {
   description = "(Required) Specifies the name of the Service Bus Queue resource. Changing this forces a new resource to be created."
 }
 
-variable resource_group_name {
+variable namespace_id {
   type        = string
-  description = "(Required) The name of the resource group in which to create the queue. Changing this forces a new resource to be created."
-}
-
-variable namespace_name {
-  type        = string
-  description = "(Required) The name of the Service Bus Namespace to create this queue in. Changing this forces a new resource to be created."
+  description = "(Required) The ID of the ServiceBus Namespace to create this queue in. Changing this forces a new resource to be created."
 }
 
 variable requires_session {
