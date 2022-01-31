d182a6bb1937d68c81da390457e62c8d50952ee6
Dan Stenrøjl <XDAST@energinet.dk>
2022-01-31 11:38:50 +0100

    WHat: Update dependencies

--- azure/function-app/outputs.tf
+++ azure/function-app/outputs.tf
@@ -14,12 +14,14 @@
 output "id" {
   value       = azurerm_function_app.this.id
   description = "The ID of the Function App."
-  
+
   depends_on  = [
     azurerm_storage_account.this,
     azurerm_storage_account_network_rules.this,
-    azurerm_private_endpoint.this,
-    azurerm_private_dns_a_record.this,
+    azurerm_private_endpoint.blob,
+    azurerm_private_dns_a_record.blob,
+    azurerm_private_endpoint.file,
+    azurerm_private_dns_a_record.file,
     azurerm_app_service_virtual_network_swift_connection.this,
     azurerm_function_app.this,
   ]
@@ -28,12 +30,14 @@ output "id" {
 output "name" {
   value       = azurerm_function_app.this.name
   description = "The name of the Function App."
-  
+
   depends_on  = [
     azurerm_storage_account.this,
     azurerm_storage_account_network_rules.this,
-    azurerm_private_endpoint.this,
-    azurerm_private_dns_a_record.this,
+    azurerm_private_endpoint.blob,
+    azurerm_private_dns_a_record.blob,
+    azurerm_private_endpoint.file,
+    azurerm_private_dns_a_record.file,
     azurerm_app_service_virtual_network_swift_connection.this,
     azurerm_function_app.this,
   ]
@@ -42,12 +46,14 @@ output "name" {
 output "default_hostname" {
   value       = azurerm_function_app.this.default_hostname
   description = "The default hostname associated with the Function App - such as mysite.azurewebsites.net"
-  
+
   depends_on  = [
     azurerm_storage_account.this,
     azurerm_storage_account_network_rules.this,
-    azurerm_private_endpoint.this,
-    azurerm_private_dns_a_record.this,
+    azurerm_private_endpoint.blob,
+    azurerm_private_dns_a_record.blob,
+    azurerm_private_endpoint.file,
+    azurerm_private_dns_a_record.file,
     azurerm_app_service_virtual_network_swift_connection.this,
     azurerm_function_app.this,
   ]
@@ -56,12 +62,14 @@ output "default_hostname" {
 output "outbound_ip_addresses" {
   value       = azurerm_function_app.this.outbound_ip_addresses
   description = "A comma separated list of outbound IP addresses - such as 52.23.25.3,52.143.43.12"
-  
+
   depends_on  = [
     azurerm_storage_account.this,
     azurerm_storage_account_network_rules.this,
-    azurerm_private_endpoint.this,
-    azurerm_private_dns_a_record.this,
+    azurerm_private_endpoint.blob,
+    azurerm_private_dns_a_record.blob,
+    azurerm_private_endpoint.file,
+    azurerm_private_dns_a_record.file,
     azurerm_app_service_virtual_network_swift_connection.this,
     azurerm_function_app.this,
   ]
@@ -70,12 +78,14 @@ output "outbound_ip_addresses" {
 output "possible_outbound_ip_addresses" {
   value       = azurerm_function_app.this.possible_outbound_ip_addresses
   description = "A comma separated list of outbound IP addresses - such as 52.23.25.3,52.143.43.12,52.143.43.17 - not all of which are necessarily in use. Superset of outbound_ip_addresses"
-  
+
   depends_on  = [
     azurerm_storage_account.this,
     azurerm_storage_account_network_rules.this,
-    azurerm_private_endpoint.this,
-    azurerm_private_dns_a_record.this,
+    azurerm_private_endpoint.blob,
+    azurerm_private_dns_a_record.blob,
+    azurerm_private_endpoint.file,
+    azurerm_private_dns_a_record.file,
     azurerm_app_service_virtual_network_swift_connection.this,
     azurerm_function_app.this,
   ]
@@ -84,12 +94,14 @@ output "possible_outbound_ip_addresses" {
 output "identity" {
   value       = azurerm_function_app.this.identity
   description = "An identity block as defined below, which contains the Managed Service Identity information for this App Service."
-  
+
   depends_on  = [
     azurerm_storage_account.this,
     azurerm_storage_account_network_rules.this,
-    azurerm_private_endpoint.this,
-    azurerm_private_dns_a_record.this,
+    azurerm_private_endpoint.blob,
+    azurerm_private_dns_a_record.blob,
+    azurerm_private_endpoint.file,
+    azurerm_private_dns_a_record.file,
     azurerm_app_service_virtual_network_swift_connection.this,
     azurerm_function_app.this,
   ]
@@ -98,12 +110,14 @@ output "identity" {
 output "site_credential" {
   value       = azurerm_function_app.this.site_credential
   description = "A site_credential block as defined below, which contains the site-level credentials used to publish to this App Service."
-  
+
   depends_on  = [
     azurerm_storage_account.this,
     azurerm_storage_account_network_rules.this,
-    azurerm_private_endpoint.this,
-    azurerm_private_dns_a_record.this,
+    azurerm_private_endpoint.blob,
+    azurerm_private_dns_a_record.blob,
+    azurerm_private_endpoint.file,
+    azurerm_private_dns_a_record.file,
     azurerm_app_service_virtual_network_swift_connection.this,
     azurerm_function_app.this,
   ]
@@ -112,12 +126,14 @@ output "site_credential" {
 output "kind" {
   value       = azurerm_function_app.this.kind
   description = "The Function App kind - such as functionapp,linux,container"
-  
+
   depends_on  = [
     azurerm_storage_account.this,
     azurerm_storage_account_network_rules.this,
-    azurerm_private_endpoint.this,
-    azurerm_private_dns_a_record.this,
+    azurerm_private_endpoint.blob,
+    azurerm_private_dns_a_record.blob,
+    azurerm_private_endpoint.file,
+    azurerm_private_dns_a_record.file,
     azurerm_app_service_virtual_network_swift_connection.this,
     azurerm_function_app.this,
   ]
