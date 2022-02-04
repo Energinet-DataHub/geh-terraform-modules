413ca8227b37b043b3b3f70e76a0b2b43241feb5
djorgensendk <32477395+djorgensendk@users.noreply.github.com>
2022-02-04 14:11:29 +0100

    Add private endpoint to eventhub namespaces (#49)

--- azure/eventhub-namespace/main.tf
+++ azure/eventhub-namespace/main.tf
@@ -13,7 +13,7 @@
 # limitations under the License.
 locals {
   module_tags = {
-    "ModuleVersion" = "5.1.0"
+    "ModuleVersion" = "6.0.0"
     "ModuleId"      = "azure-eventhub-namespace"
   }
 }
@@ -34,4 +34,49 @@ resource "azurerm_eventhub_namespace" "this" {
       tags,
     ]
   }
+
+  depends_on          = [
+    var.private_endpoint_subnet_id
+  ]
+}
+
+resource "random_string" "this" {
+  length  = 5
+  special = false
+  upper   = false
+}
+
+resource "azurerm_private_endpoint" "this" {
+  name                = "pe-${lower(var.name)}${random_string.this.result}-${lower(var.project_name)}-${lower(var.environment_short)}-${lower(var.environment_instance)}"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  subnet_id           = var.private_endpoint_subnet_id
+
+  private_service_connection {
+    name                            = "psc-01"
+    private_connection_resource_id  = azurerm_servicebus_namespace.this.id
+    is_manual_connection            = false
+    subresource_names               = ["namespace"]
+  }
+
+  tags                              = merge(var.tags, local.module_tags)
+
+  lifecycle {
+    ignore_changes = [
+      # Ignore changes to tags, e.g. because a management agent
+      # updates these based on some ruleset managed elsewhere.
+      tags,
+    ]
+  }
+}
+
+# Create an A record pointing to the namespace private endpoint
+resource "azurerm_private_dns_a_record" "this" {
+  name                = azurerm_eventhub_namespace.this.name
+  zone_name           = "privatelink.servicebus.windows.net"
+  resource_group_name = var.private_dns_resource_group_name
+  ttl                 = 3600
+  records             = [
+    azurerm_private_endpoint.this.private_service_connection[0].private_ip_address
+  ]
 }
