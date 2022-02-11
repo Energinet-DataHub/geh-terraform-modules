16456afa9a3f1c4fb9aa28394e77d91481903ce9
Jens Riis <jens.j.riis@gmail.com>
2022-02-11 11:56:38 +0100

    What: Log Workspace

--- azure/log-workspace/main.tf
+++ azure/log-workspace/main.tf
@@ -16,6 +16,6 @@ resource "azurerm_log_analytics_workspace" "this" {
   resource_group_name = var.resource_group_name
   sku                 = var.sku
   retention_in_days   = var.retention_in_days  < 30  ? 30 : var.retention_in_days
-  tags                = azurerm_resource_group.this.tags
+  tags                = merge(var.tags, local.module_tags)
 }
 
