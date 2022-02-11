a858024160309744e72fa600ba6f77a945716b40
Jens Riis <jens.j.riis@gmail.com>
2022-02-11 11:21:47 +0100

    What: Log Workspace

---
+++ azure/log-workspace/main.tf
@@ -0,0 +1,21 @@
+
+
+
+locals {
+  module_tags = {
+    "ModuleVersion" = "6.0.0"
+    "ModuleId"      = "azure-log-workspace"
+  }
+}
+
+data "azurerm_client_config" "this" {}
+
+resource "azurerm_log_analytics_workspace" "this" {
+  name                = "log-${var.name}-${lower(var.domain_name_short)}-${lower(var.environment_short)}-${lower(var.environment_instance)}"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  sku                 = var.sku
+  retention_in_days   = var.retention_in_days  < 30  ? 30 : var.retention_in_days
+  tags                = azurerm_resource_group.this.tags
+}
+
