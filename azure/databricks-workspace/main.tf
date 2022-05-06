249f709946d284f858a500ea1a8e1df16935f304
Daniel Bjerring Jørgensen <dbj1986dk@gmail.com>
2022-05-06 12:57:51 +0200

    re-add databricks pdnsz etc

--- azure/databricks-workspace/main.tf
+++ azure/databricks-workspace/main.tf
@@ -134,3 +134,39 @@ resource "azurerm_virtual_network_peering" "remote" {
   resource_group_name           = var.main_virtual_network_resource_group_name
   allow_virtual_network_access  = true
 }
+
+# Create a Private DNS Zone for Data Lake File System Gen2 (DFS) resources
+resource "azurerm_private_dns_zone" "dfs" {
+  name                = "privatelink.dfs.core.windows.net"
+  resource_group_name = var.resource_group_name
+
+  tags                = var.tags
+}
+
+# The Private DNS Zone for DFS must be linked with the Databricks dedicated virtual network
+resource "azurerm_private_dns_zone_virtual_network_link" "dfs" {
+  name                  = "pdnsz-dfs-${lower(var.project_name)}-${lower(var.environment_short)}-${lower(var.environment_instance)}"
+  resource_group_name   = var.resource_group_name
+  private_dns_zone_name = azurerm_private_dns_zone.dfs.name
+  virtual_network_id    = azurerm_virtual_network.this.id
+
+  tags                  = var.tags
+}
+
+# Create a Private DNS Zone for Blob resources
+resource "azurerm_private_dns_zone" "blob" {
+  name                = "privatelink.blob.core.windows.net"
+  resource_group_name = var.resource_group_name
+
+  tags                = var.tags
+}
+
+# The Private DNS Zone for Blob must be linked with the Databricks dedicated virtual network
+resource "azurerm_private_dns_zone_virtual_network_link" "blob" {
+  name                  = "pdnsz-blob-${lower(var.project_name)}-${lower(var.environment_short)}-${lower(var.environment_instance)}"
+  resource_group_name   = var.resource_group_name
+  private_dns_zone_name = azurerm_private_dns_zone.blob.name
+  virtual_network_id    = azurerm_virtual_network.this.id
+
+  tags                  = var.tags
+}
