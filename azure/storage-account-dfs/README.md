882467c52b7601edd28dff802c2929d3e3d2244d
Dan Stenrøjl <XDAST@energinet.dk>
2022-05-10 14:12:45 +0200

    Storage account with DFS

---
+++ azure/storage-account-dfs/README.md
@@ -0,0 +1,64 @@
+# Azure Storage Account with Data Lake File System Gen2 subresource
+
+- [Resources Created](#resources-created)
+- [Prerequisites](#prerequisites)
+- [Arguments and defaults](#arguments-and-defaults)
+- [Usage](#usage)
+- [Outputs](#outputs)
+
+## Resources Created
+
+This module creates the following resources:
+
+- [Azure Storage Account](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/storage_account)
+- [Azure Storage Account Network Rules](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/storage_account_network_rules)
+- [Azure Private Endpoint](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/private_endpoint)
+- [Azure Private DNS A Record](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/private_dns_a_record)
+- [Azure Monitor Diagnostic Setting](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/monitor_diagnostic_setting)
+
+## Prerequisites
+
+- Terraform version 1.1.7+
+- AzureRM provider version 2.97.0+
+
+## Arguments and defaults
+
+See [variables.tf](./variables.tf)
+
+## Usage
+
+```ruby
+module "storage_account_dfs_example" {
+  source                          = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/storage-account-dfs?ref=6.0.0"
+
+  name                            = "example-name"
+  project_name                    = "example-project-name"
+  environment_short               = "u"
+  environment_instance            = "001"
+  resource_group_name             = "example-resource-group-name"
+  location                        = "westeurope"
+  private_endpoint_subnet_id      = "example-private-endpoint-subnet-id"
+  account_tier                    = "Standard"
+  account_replication_type        = "LRS"
+  is_hns_enabled                  = true
+  log_analytics_workspace_id      = "example-log-analytics-workspace"
+  private_dns_resource_group_name = "example-private-dns-resource-group-name"
+
+  tags                            = {}
+}
+```
+
+Two tags are added by default:
+
+```ruby
+locals {
+  module_tags = {
+    "ModuleVersion" = "6.0.0"
+    "ModuleId"      = "azure-storage-account-dfs"
+  }
+}
+```
+
+## Outputs
+
+See [outputs.tf](./outputs.tf)
---
+++ azure/storage-account-dfs/variables.tf
@@ -0,0 +1,103 @@
+# Copyright 2020 Energinet DataHub A/S
+#
+# Licensed under the Apache License, Version 2.0 (the "License2");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+terraform {
+  experiments = [
+    module_variable_optional_attrs,
+  ]
+}
+
+variable name {
+  type        = string
+  description = "(Required) Specifies the name of the storage account. Changing this forces a new resource to be created. This must be unique across the entire Azure service, not just within the resource group."
+}
+
+variable project_name {
+  type          = string
+  description   = "(Required) Name of the project this infrastructure is a part of."
+}
+
+variable environment_short {
+  type        = string
+  description = "(Required) The short value name of the environment."
+}
+
+variable environment_instance {
+  type        = string
+  description = "(Required) The instance value of the environment."
+}
+
+variable resource_group_name {
+  type        = string
+  description = "(Required) The name of the resource group in which the resources are created. Changing this forces a new resource to be created."
+}
+
+variable location {
+  type        = string
+  description = "(Required) The Azure region where the resources are created. Changing this forces a new resource to be created."
+}
+
+variable private_endpoint_subnet_id {
+  type        = string
+  description = "(Required) The ID of the Subnet from which Private IP Addresses will be allocated for Private Endpoints. Changing this forces a new resource to be created."
+}
+
+variable account_tier {
+  type        = string
+  description = "(Required) Defines the Tier to use for this storage account. Valid options are Standard and Premium. For BlockBlobStorage and FileStorage accounts only Premium is valid. Changing this forces a new resource to be created."
+}
+
+variable account_replication_type {
+  type        = string
+  description = "(Required) Defines the type of replication to use for this storage account. Valid options are LRS, GRS, RAGRS, ZRS, GZRS and RAGZRS."
+}
+
+variable access_tier {
+  type        = string
+  description = "(Optional) Defines the access tier for BlobStorage, FileStorage and StorageV2 accounts. Valid options are Hot and Cool, defaults to Hot."
+  default     = "Hot"
+}
+
+variable is_hns_enabled {
+  type        = bool
+  description = "(Optional) Is Hierarchical Namespace enabled? This can be used with Azure Data Lake Storage Gen 2 (see here for more information - https://docs.microsoft.com/en-us/azure/storage/blobs/data-lake-storage-quickstart-create-account/). Changing this forces a new resource to be created."
+  default     = false
+}
+
+variable account_kind {
+  type        = string
+  description = "(Optional) Defines the Kind of account. Valid options are BlobStorage, BlockBlobStorage, FileStorage, Storage and StorageV2. Changing this forces a new resource to be created. Defaults to StorageV2."
+  default     = "StorageV2"
+}
+
+variable log_analytics_workspace_id {
+  type = string
+  description = "(Required) The id of the Log Analytics Workspace where the Storage Account will log events (e.g. audit events)"
+}
+
+variable log_retention_in_days {
+  type        = number
+  description = "(Optional) The number of days for which this Retention Policy should apply."
+  default     = 183
+}
+
+variable private_dns_resource_group_name {
+  type        = string
+  description = "(Required) Specifies the resource groups where the Private DNS Zones exists. Changing this forces a new resource to be created."
+}
+
+variable tags {
+  type        = any
+  description = "(Optional) A mapping of tags to assign to the resources."
+  default     = {}
+}
