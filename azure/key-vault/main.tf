613577acb6806e164d1fb79edc406af29345a855
René Nielsen <renetdev@gmail.com>
2022-05-16 14:09:08 +0200

    try remove dns group from key vualt

--- azure/key-vault/main.tf
+++ azure/key-vault/main.tf
@@ -74,7 +74,6 @@ resource "azurerm_private_endpoint" "this" {
       # Ignore changes to tags, e.g. because a management agent
       # updates these based on some ruleset managed elsewhere.
       tags,
-      private_dns_zone_group,
     ]
   }
 }
